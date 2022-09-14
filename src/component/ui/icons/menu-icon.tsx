154eb5016f0d51baf7c34c086ecb9a60aa2e2dc2
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-15 06:24:15 +0700

    feat: add menu icon

---
+++ src/component/ui/icons/menu-icon.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps } from "react";
+
+export function MenuIcon({
+  width = "192",
+  height = "192",
+  fill = "white",
+  ...props
+}: ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill={fill}
+      viewBox="0 0 256 256"
+      {...props}
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <path d="M216,120H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
+      <path d="M40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16Z"></path>
+      <path d="M216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
+    </svg>
+  );
+}

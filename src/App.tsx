2d9427003cccfca03b9771a860ee7c02b9670d2b
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-15 22:13:35 +0700

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { css } from "@emotion/css";
-import { Global } from "@emotion/react";
 import { Outlet } from "react-router-dom";
-import Navigation from "src/component/layout/navigation";
-import normalizeCss from "src/styles/normalize-css";
-import Footer from "./component/layout/footer";
-import theme from "./styles/theme";
+import Layout from "./component/layout";
 
 function App() {
   return (
-    <div
-      className={css`
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-        background-color: ${theme.colors.black};
-      `}
-    >
-      <Global styles={normalizeCss} />
-      <Navigation />
-      <main
-        className={css`
-          flex-grow: 1;
-        `}
-      >
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <Outlet />
+    </Layout>
   );
 }
 
---
+++ src/component/layout/index.tsx
@@ -0,0 +1,35 @@
+import { css } from "@emotion/css";
+import { Global } from "@emotion/react";
+import { ReactNode } from "react";
+import normalizeCss from "src/styles/normalize-css";
+import theme from "src/styles/theme";
+import Footer from "./footer";
+import Navigation from "./navigation";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div
+      className={css`
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        background-color: ${theme.colors.black};
+      `}
+    >
+      <Global styles={normalizeCss} />
+      <Navigation />
+      <main
+        className={css`
+          flex-grow: 1;
+        `}
+      >
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}

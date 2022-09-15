72c2074c5bc619b5200a9bb7e777455acf16ef39
fikrialwan <fikriar93@gmail.com>
2022-09-15 11:56:17 +0700

    feat: remove etc resize function

--- src/etc/resize-window.ts
+++
@@ -1,7 +0,0 @@
-const resizeWindow = (width: number, height: number) => {
-  window.innerWidth = width;
-  window.innerHeight = height;
-  window.dispatchEvent(new Event("resize"));
-};
-
-export default resizeWindow;

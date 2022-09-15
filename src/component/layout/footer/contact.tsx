60904bab5eed2f15d54460a08df8f48088e55c31
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-15 21:16:57 +0700

    feat: add contact footer content

--- src/component/layout/footer/contact.tsx
+++ src/component/layout/footer/contact.tsx
@@ -1,3 +1,20 @@
+import getInTouchIlustration from "src/component/ui/images/get-in-touch-ilustration.webp";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <article>
+        <h2>Get In Touch</h2>
+        <p>
+          Although I'm not currently looking for any new opportunities, my inbox
+          is always open. Whether you have a question or just want to say hello,
+          I'll try my best to get back to you!
+        </p>
+        <a href="mailto:fikriar93@gmail.com">Say hello</a>
+      </article>
+      <div>
+        <img src={getInTouchIlustration} alt="get in touch ilustration" />
+      </div>
+    </section>
+  );
 }

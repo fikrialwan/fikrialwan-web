328f15262b681e9943c82983b1669b3e361d04cf
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-15 19:38:40 +0700

    feat: add copyright footer

--- src/component/layout/footer/index.tsx
+++ src/component/layout/footer/index.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/css";
 import theme from "src/styles/theme";
+import Copyright from "./copyright";
 
 export default function Footer() {
   return (
@@ -9,7 +10,7 @@ export default function Footer() {
         color: ${theme.colors.white};
       `}
     >
-      ini halaman footer
+      <Copyright />
     </footer>
   );
 }

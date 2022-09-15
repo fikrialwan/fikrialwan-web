cefbef662c1eb75169859ad99328f25a466ea809
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-15 19:50:32 +0700

    feat: add copyright content

--- src/component/layout/footer/copyright.tsx
+++ src/component/layout/footer/copyright.tsx
@@ -1,3 +1,42 @@
 export default function Copyright() {
-  return <div>Copyright</div>;
+  return (
+    <div>
+      <p>
+        Inspired by
+        <a
+          href="https://kentcdodds.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          https://kentcdodds.com
+        </a>
+        ,
+        <a
+          href="https://sonnylab.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          https://sonnylab.com
+        </a>{" "}
+        and
+        <a
+          href="https://brittanychiang.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          https://brittanychiang.com
+        </a>
+      </p>
+      <p>
+        Designed and built by{" "}
+        <a
+          href="https://www.linkedin.com/in/fikri-alwan/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Fikri Alwan Ramadhan
+        </a>
+      </p>
+    </div>
+  );
 }

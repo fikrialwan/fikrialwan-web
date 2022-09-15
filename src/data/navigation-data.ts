92a4266edc4409ec978769acecc7b08d98f28e69
fikrialwan <fikriar93@gmail.com>
2022-09-15 12:22:00 +0700

    feat: add navigation data and interface for navigation data

---
+++ src/data/navigation-data.ts
@@ -0,0 +1,19 @@
+export interface NavigationDataType {
+  title: string;
+  link: string;
+}
+
+export const navigationData: NavigationDataType[] = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Projects",
+    link: "/projects",
+  },
+];

968dfe8dc52872adbae9cb56ba58ee02b54d66b7
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 19:05:44 +0700

    feat: add arrow right icon

---
+++ src/component/ui/icons/arrow-right-icon.tsx
@@ -0,0 +1,35 @@
+import { ComponentProps } from "react";
+
+export default function ArrowRightIcon({
+  width = "20",
+  height = "17",
+  fill = "white",
+  ...props
+}: ComponentProps<"svg">) {
+  return (
+    <svg
+      data-testid="arrow-right-icon"
+      width={width}
+      height={height}
+      viewBox="0 0 20 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M1 8.5H19"
+        stroke={fill}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.6365 1.13635L19.0001 8.49999L11.6365 15.8636"
+        stroke={fill}
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}

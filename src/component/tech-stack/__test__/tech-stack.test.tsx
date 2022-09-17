44796371950f4785739e9be53b75dd97e4ecabcd
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 19:51:42 +0700

    test: add test case for tach stack component

---
+++ src/component/tech-stack/__test__/tech-stack.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { aboutHomeData } from "src/data";
+import TechStack from "..";
+
+describe("Tech stack", () => {
+  it("Render correctly", () => {
+    render(<TechStack data={aboutHomeData.techStack} />);
+
+    aboutHomeData.techStack?.forEach((tech) => {
+      const techItem = screen.getByText(tech);
+      expect(techItem).toBeVisible();
+    });
+  });
+});

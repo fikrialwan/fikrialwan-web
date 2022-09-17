e59d64e2bde507c3786ff44b9e44950d78d35f19
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 20:02:59 +0700

    feat: add content and style tech stack component

--- src/component/tech-stack/index.tsx
+++ src/component/tech-stack/index.tsx
@@ -1,3 +1,38 @@
-export default function TechStack() {
-  return <div>TechStack</div>;
+import { css } from "@emotion/css";
+import theme from "src/styles/theme";
+
+interface TechStackProps {
+  data: string[];
+}
+
+export default function TechStack({ data }: TechStackProps) {
+  return (
+    <ul
+      className={css`
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        padding-left: 0;
+        gap: 10px;
+      `}
+    >
+      {data.map((tech: string, key: number) => {
+        return (
+          <li
+            className={css`
+              color: ${theme.colors.blue};
+              border: 1px solid ${theme.colors.blue};
+              border-radius: 25px;
+              padding: 5px 15px;
+              font-weight: 400;
+              font-size: 20px;
+            `}
+            key={key}
+          >
+            {tech}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }

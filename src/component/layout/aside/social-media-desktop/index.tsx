3d2a1ca73448951b649d583b3168c91e14f6140e
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 08:29:25 +0700

    feat: add style to social media desktop size aside

--- src/component/layout/aside/social-media-desktop/index.tsx
+++ src/component/layout/aside/social-media-desktop/index.tsx
@@ -1,26 +1,92 @@
+import { css } from "@emotion/css";
+import mq from "src/styles/breakpoints";
+import github from "src/component/ui/images/github.png";
+import linkedin from "src/component/ui/images/linkedin.png";
+import theme from "src/styles/theme";
+
 export default function SocialMediaDesktop() {
   return (
-    <aside>
-      <address>
+    <aside
+      className={css`
+        display: none;
+        ${mq[1]} {
+          display: flex;
+        }
+      `}
+    >
+      <address
+        className={css`
+          left: 50px;
+          bottom: 0;
+          position: fixed;
+          height: 50vh;
+          display: flex;
+          flex-direction: column;
+          gap: 30px;
+          align-items: center;
+          ${mq[2]} {
+            left: 75px;
+          }
+        `}
+      >
         <a
           href="https://github.com/fikrialwan"
           target="_blank"
           rel="noreferrer noopener"
         >
-          <img src="/assets/images/github.png" alt="github icon" />
+          <img src={github} alt="Github icon" />
         </a>
         <a
           href="https://www.linkedin.com/in/fikri-alwan/"
           target="_blank"
           rel="noreferrer noopener"
         >
-          <img src="/assets/images/linkedin.png" alt="linkedin icon" />
+          <img src={linkedin} alt="Linkedin icon" />
         </a>
-        <div></div>
+        <div
+          className={css`
+            flex: 1;
+            width: 1px;
+            background-color: ${theme.colors.white};
+          `}
+        />
       </address>
-      <address>
-        <a href="mailto:fikriar93@gmail.com">fikriar93@gmail.com</a>
-        <div></div>
+      <address
+        className={css`
+          right: 50px;
+          bottom: 0;
+          position: fixed;
+          height: 50vh;
+          display: flex;
+          flex-direction: column;
+          gap: 30px;
+          align-items: center;
+          ${mq[2]} {
+            right: 75px;
+          }
+        `}
+      >
+        <a
+          href="mailto:fikriar93@gmail.com"
+          className={css`
+            font-size: 20px;
+            font-weight: 600;
+            font-style: normal;
+            text-decoration: none;
+            color: ${theme.colors.white};
+            writing-mode: vertical-rl;
+            text-orientation: mixed;
+          `}
+        >
+          fikriar93@gmail.com
+        </a>
+        <div
+          className={css`
+            flex: 1;
+            width: 1px;
+            background-color: ${theme.colors.white};
+          `}
+        />
       </address>
     </aside>
   );

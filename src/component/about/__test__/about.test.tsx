5e94100cc408de2dc2e5ce4f0ec74ebd24796ac1
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 20:24:09 +0700

    test: complete about test case

--- src/component/about/__test__/about.test.tsx
+++ src/component/about/__test__/about.test.tsx
@@ -1,15 +1,39 @@
 import { render, screen } from "@testing-library/react";
+import { aboutData, AboutDataType, aboutHomeData } from "src/data";
 import About from "..";
 
 describe("About", () => {
   it("Render correctly on home page", () => {
-    render(<About />);
+    render(<About isHome={true} data={aboutHomeData} />);
 
     const title = screen.getByText("About me");
     expect(title).toBeVisible();
+
+    aboutHomeData.desc.split("\n").forEach((desc) => {
+      const descAbout = screen.getByText(desc);
+      expect(descAbout).toBeVisible();
+    });
+
+    aboutHomeData.techStack!.forEach((tech) => {
+      const techAbout = screen.getByText(tech);
+      expect(techAbout).toBeVisible();
+    });
+
+    const img = screen.getByAltText(aboutHomeData.img.alt);
+    expect(img).toBeVisible();
   });
 
-  it("Render correctly on about page", () => {
-    render(<About />);
+  aboutData.forEach((about: AboutDataType, key: number) => {
+    it(`Render correctly about ${key + 1} on about page`, () => {
+      render(<About isHome={false} data={about} />);
+
+      about.desc.split("\n").forEach((desc) => {
+        const descAbout = screen.getByText(desc);
+        expect(descAbout).toBeVisible();
+      });
+
+      const img = screen.getByAltText(about.img.alt);
+      expect(img).toBeVisible();
+    });
   });
 });

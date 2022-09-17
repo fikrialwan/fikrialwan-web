97700d2a071103ee2613279816bf1a92394a25fd
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 07:57:54 +0700

    feat: add social media aside for desktop size content

--- src/component/layout/aside/social-media-desktop/index.tsx
+++ src/component/layout/aside/social-media-desktop/index.tsx
@@ -1,3 +1,27 @@
 export default function SocialMediaDesktop() {
-  return <div>SocialMediaDesktop</div>;
+  return (
+    <aside>
+      <address>
+        <a
+          href="https://github.com/fikrialwan"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src="/assets/images/github.png" alt="github icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/fikri-alwan/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src="/assets/images/linkedin.png" alt="linkedin icon" />
+        </a>
+        <div></div>
+      </address>
+      <address>
+        <a href="mailto:fikriar93@gmail.com">fikriar93@gmail.com</a>
+        <div></div>
+      </address>
+    </aside>
+  );
 }

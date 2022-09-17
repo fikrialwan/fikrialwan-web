46f85746ac6ba124e02566fde15e40c201e235a7
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-17 10:34:12 +0700

    feat: add hero props

--- src/component/hero/hero.tsx
+++ src/component/hero/hero.tsx
@@ -1,3 +1,28 @@
-export default function Hero() {
-  return <div>Hero</div>;
+import { HeroContentType } from "src/data";
+
+interface HeroPropsType {
+  isHome: Boolean;
+  data: HeroContentType;
+}
+
+export default function Hero({ isHome, data }: HeroPropsType) {
+  if (isHome) {
+    const {
+      greeting,
+      title: heroTitle,
+      desc,
+      descHighlight,
+      img: { url: imgUrl, alt: imgAlt },
+      button,
+    } = data;
+    const { title: buttonTitle, link: buttonLink } = button!;
+    return <section></section>;
+  } else {
+    const {
+      title,
+      desc,
+      img: { url: imgUrl, alt: imgAlt },
+    } = data;
+    return <section></section>;
+  }
 }

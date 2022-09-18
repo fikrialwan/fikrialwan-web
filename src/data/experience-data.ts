4b3d1f6187d4dfe6c9149eab0f591727b1729aa8
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-18 16:14:21 +0700

    feat: add experience data

---
+++ src/data/experience-data.ts
@@ -0,0 +1,28 @@
+export interface ExperienceDataType {
+  company: string;
+  position: string;
+  desc: string;
+}
+
+export const experienceData: ExperienceDataType[] = [
+  {
+    company: "PT. Netta Code Indonesia",
+    position: "Mobile Developer",
+    desc: "I am working in PT. Nettacode Indonesia as a mobile developer since September 2021 until now. I am developing an android app named PIFA and publish it in playstore. I developed that app using dart programming language and flutter SDK.",
+  },
+  {
+    company: "Catamyst x Codepolitan",
+    position: "Student",
+    desc: "I'm taking the catamyst x codepolitan front-end bootcamp class from august 2022 until october 2022. In this bootcamp I learn front-end web development from zero. I learn how to create markdown, design UI in Figma, and learn how to develop a website using HTML, CSS, javascript, and React.",
+  },
+  {
+    company: "Tanjungpura University",
+    position: "Student",
+    desc: "I am an undergraduate student at Tanjungpura Unversity majoring in computer system engineering since July 2016 and will be finished on October 2022. when I was in college I focused on studying mobile and web development. My final project research made a website to detect clickbait news from news titles using Multinomial Naive Bayes algorithm.",
+  },
+  {
+    company: "Bersihberes",
+    position: "Mobile Application Developer",
+    desc: "I am working in Bersihberes as a mobile application developer since July 2020 until January 2021. I am developing an android app named PIFA and publish it in playstore. I developed that app using dart programming language and flutter SDK.",
+  },
+];

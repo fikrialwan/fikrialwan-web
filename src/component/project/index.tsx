4b57c54eb7eba86517f0e34ba481f8a8f710802d
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-22 18:27:45 +0700

    feat: add project section in project page

--- src/component/project/index.tsx
+++ src/component/project/index.tsx
@@ -161,6 +161,126 @@ export default function ProjectSection({ isHome }: ProjectSectionProps) {
       </section>
     );
   } else {
-    return <div>projects</div>;
+    return (
+      <section
+        className={css`
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+          gap: 20px;
+          margin-bottom: 15px;
+        `}
+      >
+        {projectsData.map(
+          ({ name, desc, techStack, url }: ProjectsDataType, key: number) => {
+            return (
+              <article
+                key={key}
+                className={css`
+                  display: flex;
+                  flex-direction: column;
+                  padding: 15px;
+                  background-color: ${theme.colors.blackGrey};
+                  border-radius: 18px;
+                  transition: all 0.5ms ease-in;
+                  &:hover {
+                    outline-offset: 4px;
+                    outline: 3px solid ${theme.colors.white};
+                  }
+                `}
+              >
+                <div
+                  className={css`
+                    display: flex;
+                    justify-content: end;
+                    align-items: center;
+                  `}
+                >
+                  {url.map((urlData: LinkProjectsType, key: number) => {
+                    if (urlData.type === LinkProjects.github) {
+                      return (
+                        <a
+                          key={key}
+                          href={urlData.url}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          <GithubIcon
+                            width={30}
+                            height={30}
+                            className={css`
+                              margin-left: 5px;
+                              cursor: pointer;
+                            `}
+                          />
+                        </a>
+                      );
+                    } else if (urlData.type === LinkProjects.link) {
+                      return (
+                        <a
+                          key={key}
+                          href={urlData.url}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          <LinkIcon
+                            width={25}
+                            height={25}
+                            className={css`
+                              margin-left: 5px;
+                              cursor: pointer;
+                            `}
+                          />
+                        </a>
+                      );
+                    } else {
+                      return (
+                        <a
+                          key={key}
+                          href={urlData.url}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          <FigmaIcon
+                            width={30}
+                            height={30}
+                            className={css`
+                              margin-left: 5px;
+                              cursor: pointer;
+                            `}
+                          />
+                        </a>
+                      );
+                    }
+                  })}
+                </div>
+                <h3
+                  className={css`
+                    font-size: 24px;
+                    font-weight: 600;
+                    color: ${theme.colors.white};
+                    margin: 10px 0 0 0;
+                    padding: 0;
+                  `}
+                >
+                  {name}
+                </h3>
+                <p
+                  className={css`
+                    font-size: 16px;
+                    font-weight: 400;
+                    flex: 1;
+                    color: ${theme.colors.grey};
+                    margin: 16px 0;
+                  `}
+                >
+                  {desc}
+                </p>
+                <TechStack data={techStack} />
+              </article>
+            );
+          }
+        )}
+      </section>
+    );
   }
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,12 @@
 import Hero from "src/component/hero/hero";
+import ProjectSection from "src/component/project";
 import { heroData } from "src/data";
 
 function Projects() {
   return (
     <>
       <Hero isHome={false} data={heroData.projects} />
+      <ProjectSection isHome={false} />
     </>
   );
 }
